2854b81281613dd435363cb13f087c7ba573cc23
IlNat <ilanatalcenko18@gmail.com>
2022-04-18 20:17:42 +0500

    Add code(print, is_full)

---
+++ NatIlyaTTT.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using std::cout;
+void print(const char board[3][3]);
+
+int main()
+{
+    
+}
---
+++ TTT_script.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using std::cout;
+
+char board[3][3];
+enum { EMPTY = ' ', CROSS = 'X', ZERO = 'O' };
+
+void print(const char board[3][3]) {
+	for (int i = 0; i < 3; i++) {
+		cout << ' ' << board[i][0];
+		for (int j = 1; j < 3; j++) {
+			cout << " | " << board[i][j];
+		}
+		cout << '\n';
+		if (i == 2) {
+			break;
+		}
+		for (int j = 0; j < 3; j++) {
+			cout << "---" << ((j == 2) ? '\n' : '+');
+		}
+	}
+}
+
+bool is_full(const char board[3][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (board[i][j] == EMPTY) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
