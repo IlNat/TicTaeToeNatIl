93858abeedd5c5aabcdcf844ae0a78abdf626c83
IlNat <ilanatalcenko18@gmail.com>
2022-04-18 20:44:41 +0500

    script - add [init, get_coords, make_move] main - nothing matter +add lib

--- NatIlyaTTT.cpp
+++ NatIlyaTTT.cpp
@@ -1,8 +1,9 @@
 ﻿#include <iostream>
+#include "TTT_lib.h"
 using std::cout;
 void print(const char board[3][3]);
 
 int main()
 {
-    
+    init();
 }
--- TTT_script.cpp
+++ TTT_script.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <cctype>
 using std::cout;
+using std::cin;
 
 char board[3][3];
 enum { EMPTY = ' ', CROSS = 'X', ZERO = 'O' };
+char player_char;
+char computer_char;
+
+void init() {
+	setlocale(LC_ALL, "russian");
+	do {
+		cout << "Сделайте свой выбор [X | O]: ";
+		cin >> player_char;
+		player_char = toupper(player_char); // 'x' -> 'X'
+	} while (player_char != 'X' && player_char != 'O');
+
+	computer_char = (player_char == 'X' ? 'O' : 'X');
+}
+
+void get_coords(int& row, int& col) {
+	do {
+		cout << "Введите координаты x: [1; 3] y: [1; 3]: ";
+		cin >> col >> row;
+		col--;
+		row--;
+	} while (col < 0 || col > 2 || row < 0 || row > 2 || board [row][col] != EMPTY);
+}
+
+void make_move() {
+	// Ход компьютера.
+}
 
 void print(const char board[3][3]) {
 	for (int i = 0; i < 3; i++) {
